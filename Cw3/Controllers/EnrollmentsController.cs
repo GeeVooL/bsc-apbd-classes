0c9b52bbc5b359da1e82d6a02a847b5577147906
Michał Dziewulski <michal@dziewulski.pl>
2020-04-08 02:39:52 +0200

    Finish task 4.3

---
+++ Cw3/Controllers/EnrollmentsController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Cw3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cw3.Controllers
+{
+    [Route("api/enrollments")]
+    [ApiController]
+    public class EnrollmentsController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public IActionResult GetEnrollment(String id)
+        {
+            var enrollments = new List<Enrollment>();
+
+            using (var connection = new SqlConnection("Data Source=db-mssql;Initial Catalog=2019SBD;Integrated Security=True"))
+            using (var command = new SqlCommand())
+            {
+                command.Connection = connection;
+                command.CommandText = @"SELECT E.IdEnrollment, E.Semester, E.StartDate
+                                        FROM Enrollment E
+                                        INNER JOIN Student S on S.IdEnrollment = E.IdEnrollment
+                                        WHERE S.IndexNumber = " + id + ";";
+                connection.Open();
+                var dataReader = command.ExecuteReader();
+                while (dataReader.Read())
+                {
+                    var enrollment = new Enrollment();
+                    enrollment.IdEnrollment = int.Parse(dataReader["IdEnrollment"].ToString());
+                    enrollment.Semester = int.Parse(dataReader["Semester"].ToString());
+                    enrollment.StartDate = DateTime.Parse(dataReader["StartDate"].ToString());
+                    enrollments.Add(enrollment);
+                }
+            }
+
+            return Ok(enrollments);
+        }
+    }
+}
